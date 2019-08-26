baf9e5dbace870bf3f4a3edf00a55ccf20848e47
juhahinkula <hinkula@gmail.com>
2019-08-26 13:08:56 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import {StyleSheet, Text, TextInput, View, Button, FlatList, Alert} from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "YOUR_URL",
+  projectId: "YOUR_PROJECTID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDERID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default function App() {
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    firebase.database().ref('items/').on('value', snapshot => {
+      const data = snapshot.val();
+      const prods = Object.values(data);
+      setItems(prods);
+    });
+  }, []);
+
+  saveItem = () => {
+    firebase.database().ref('items/').push(
+      {'product': product, 'amount': amount}
+    );
+  }
+
+  listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
+  return (  
+    <View style={styles.container}>
+      <TextInput placeholder='Product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(product) => setProduct(product)}
+        value={product}/>  
+      <TextInput placeholder='Amount' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount}/>      
+      <Button onPress={saveItem} title="Save" /> 
+      <Text style={{marginTop: 30, fontSize: 20}}>Shopping list</Text>
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
+      </View>} data={items} ItemSeparatorComponent={listSeparator} />      
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center'
+  }  
+});
---
+++ SettingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingScreen = () => {
+  return (
+    <View>
+      <Text>This is SettingScreen</Text>
+    </View>
+  );
+};
+
+SettingScreen.navigationOptions = ({navigate}) => ({title: 'Settings'});
+
+export default SettingScreen;
